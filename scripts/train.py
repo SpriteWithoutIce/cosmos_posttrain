08c851e611cce3ed8ddd9bba4c778c6878f98e7c
“coke_and_1ce” <linyihan240@gmail.com>
2026-03-22 16:21:49 +0800

    expertments

--- scripts/train.py
+++ scripts/train.py
@@ -9,7 +9,27 @@
     torchrun --nproc_per_node=1 -m scripts.train --config=configs/config.py -- experiment=my_action_experiment
 """
 
+import faulthandler
+import os
+import signal
+import sys
+
+
+def _enable_debug_stack_dump():
+    # Enable traceback dump for hard hangs:
+    #   kill -USR1 <pid>
+    faulthandler.enable(file=sys.stderr, all_threads=True)
+    faulthandler.register(signal.SIGUSR1, file=sys.stderr, all_threads=True)
+    print(
+        f"[debug] faulthandler enabled (pid={os.getpid()}). "
+        "Send SIGUSR1 to dump all thread stacks.",
+        file=sys.stderr,
+        flush=True,
+    )
+
+
 if __name__ == "__main__":
+    _enable_debug_stack_dump()
     from cosmos_oss.scripts.train import main
 
     main()
